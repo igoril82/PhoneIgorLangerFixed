585a1e142626eb383d56279872fccf7de6673c17
igoril82 <ilanger1982@gamil.com>
2022-12-22 14:57:06 +0200

    The last version

---
+++ PhoneIgorLanger/Main.java
@@ -0,0 +1,16 @@
+package PhoneIgorLanger;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        UserMenu us = new UserMenu();
+        us.userMenu();
+
+
+    }
+}
+
+
---
+++ PhoneIgorLanger/People.java
@@ -0,0 +1,37 @@
+package PhoneIgorLanger;
+
+public class People {
+    public String name;
+    public String phoneNumber;
+
+
+    public People(String nameOfContact, String numberOfContact) {
+
+    }
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String newName) {
+
+        this.name = newName;
+
+    }
+
+    public String getPhoneNumber() {
+
+        return phoneNumber;
+
+    }
+
+    public void setPhoneNumber(String newPhoneNumber) {
+
+        this.phoneNumber = newPhoneNumber;
+
+    }
+}
+
+
